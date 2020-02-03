fe77f8a8b7e2385822d09397045a994783012048
ianantonius17 <33581410+ianantonius17@users.noreply.github.com>
2020-02-03 10:31:17 -0500

    implementation + test cases

--- src/main/java/Strings/BasicStrings.java
+++ src/main/java/Strings/BasicStrings.java
@@ -3,26 +3,47 @@ package Strings;
 public class BasicStrings {
 
     public String flipConcat(String string1, String string2) {
-        return null;
+
+        return string2+string1;
     }
 
     public char getChar(String string, int index) {
-        return ' ';
+        return string.charAt(index);
     }
 
     public String iCantSee(String string) {
-        return null;
+        return string.replaceAll("[a-zA-Z']"," ");
     }
 
     public String loudAndClear(String string) {
-        return null;
+        return string.toUpperCase();
     }
 
     public String reverseCase(String string) {
-        return null;
+
+        String res = "";
+        for(int i = 0 ; i < string.length();i++){
+            int type = Character.getType(string.charAt(i));
+            if(type == Character.LOWERCASE_LETTER){
+                res+= Character.toUpperCase(string.charAt(i));
+            }
+            else
+            {
+                res+= Character.toLowerCase(string.charAt(i));
+            }
+        }
+        return res;
     }
 
     public String oneAtATime(String string1, String string2) {
-        return null;
+
+        String res = "";
+        int len = string1.length() < string2.length()?string1.length():string2.length();
+        for(int i = 0 ; i < len;i++){
+            res+= string1.charAt(i);
+            res+= string2.charAt(i);
+        }
+        res += string1.length() < string2.length()? string2.substring(len):string1.substring(len);
+        return res;
     }
 }
