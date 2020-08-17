b56c1f0b3ae8981d3d840d117c35fe532a95c04a
Mingze <sunmz1108@gmail.com>
2020-08-16 22:35:20 -0500

    allpassed

--- src/main/java/DansDilemma/DansDilemma.java
+++ src/main/java/DansDilemma/DansDilemma.java
@@ -1,16 +1,65 @@
-package DansDilemma;
+import java.util.*;
 
 public class DansDilemma {
 
     public Integer dilemmaOfTwo(Double input1, Double input2) {
-        return null;
+        Set<Double> result = new HashSet<>();
+        helper(result, input1, input2);
+        return result.size();
     }
 
     public Integer dilemmaOfThree(Double input1, Double input2, Double input3) {
-        return null;
+        Set<Double> result = new HashSet<>();
+        helper(result, input1, input2, input3);
+        return result.size();
     }
 
     public Integer dilemmaOfN(Double... args){
-        return null;
+        Set<Double> result = new HashSet<>();
+        helper(result, args);
+        return result.size();
+    }
+
+    private void helper(Set<Double> result, Double... args) {
+        Double value;
+
+        for (int i = 0; i < args.length; i++) {
+            value = args[i];
+            for (int j = 0; j < args.length; j++) {
+                if (i != j) {
+                    Double temp = value;
+                    value += args[j];
+                    result.add(value);
+                    value = temp;
+                }
+            }
+            for (int j = 0; j < args.length; j++) {
+                if (i != j) {
+                    Double temp = value;
+                    value -= args[j];
+                    result.add(value);
+                    value = temp;
+                }
+            }
+            for (int j = 0; j < args.length; j++) {
+                if (i != j) {
+                    Double temp = value;
+                    value *= args[j];
+                    result.add(value);
+                    value = temp;
+                }
+            }
+            for (int j = 0; j < args.length; j++) {
+                if (i != j) {
+                    Double temp = value;
+                    if (args[j] != 0d) {
+                        value /= args[j];
+                        result.add(value);
+                        value = temp;
+                    }
+
+                }
+            }
+        }
     }
 }
