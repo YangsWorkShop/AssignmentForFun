c212b0c7652222af5485bd491fdc5aade9f07624
Harshini Shivakumar <harshini.shivakumar@gmail.com>
2020-08-24 00:40:34 -0400

    Done by Harshini

--- src/main/java/Collections/ArrayLists.java
+++ src/main/java/Collections/ArrayLists.java
@@ -1,22 +1,57 @@
 package Collections;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class ArrayLists {
 
     public ArrayList<Integer> addShit(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        return null;
+        list1.addAll(list2);
+        return list1;
     }
 
     public Integer addShitTogether(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        return null;
+        int sum = 0;
+        for(int i : list1){
+            sum += i;
+        }
+        for(int j : list2){
+            sum += j;
+        }
+        return sum;
     }
 
     public ArrayList<Integer> removeAll(ArrayList<Integer> original, Integer toRemove) {
-        return null;
+	
+        ArrayList<Integer> new = new ArrayList<Integer>();
+        for (int n : original) {
+            if (n != toRemove) 
+		new.add(n);
+        }
+        return new;
     }
 
     public boolean happyList(ArrayList<String> original) {
-        return false;
+        if(original.size() <= 1) 
+	    return true;
+        else{
+            for(int i = 0; i < original.size()-1; i++){
+                HashSet<Character> set = new HashSet<>();
+                char[] c1 = (original.get(i)).toCharArray();
+                char[] c = (original.get(i+1)).toCharArray();
+
+                for(char ch: c1){
+                     set.add(ch);
+                }
+
+                int count = 0;
+                for(char ch : c){
+                    if(set.contains(ch)) 	
+			count++;
+                }
+                if(count == 0) 
+			return false;
+            }
+            return true;
+        }
     }
 }
--- src/main/java/DansDilemma/DansDilemma.java
+++ src/main/java/DansDilemma/DansDilemma.java
@@ -1,16 +1,40 @@
 package DansDilemma;
-
+import java.util.*;
 public class DansDilemma {
 
     public Integer dilemmaOfTwo(Double input1, Double input2) {
-        return null;
+        HashSet<Double> set=new HashSet<Double>();
+    	twoNumbersAtOnce(set,input1,input2);
+        return set.size();
     }
 
     public Integer dilemmaOfThree(Double input1, Double input2, Double input3) {
-        return null;
+        HashSet<Double> set = new HashSet<Double>();
+    	twoNumbersAtOnce(set,input1,input2);
+    	twoNumbersAtOnce(set,input2,input3);
+    	twoNumbersAtOnce(set,input1,input3);
+        return set.size();
     }
 
     public Integer dilemmaOfN(Double... args){
-        return null;
+        HashSet<Double> set = new HashSet<Double>();
+    	int l=args.length;
+        for (int i=0;i<l-1;i++) {
+            for (int j=i+1;j<l;j++) {
+            	twoNumbersAtOnce(set,args[i],args[j]);
+            }
+        }
+        return set.size();
+    }
+	
+    public void twoNumbersAtOnce(HashSet<Double> set, Double input1, Double input2) {
+        set.add(input1 + input2);
+        set.add(input1 * input2);
+        set.add(input1 - input2);
+        set.add(input2 - input1);
+        if (input2 != 0d) 
+        	set.add(input1 / input2);
+        if (input1 != 0d) 
+        	set.add(input2 / input1);
     }
 }
